e2ca69a5e932321f8a20050edfd7681dc48d1bd9
Ricardo Simão <rpsimao@gmail.com>
2015-12-12 21:41:23 +0000

    Update LanguageSelector.php
    Bug fixes

--- src/Xinax/LaravelGettext/Composers/LanguageSelector.php
+++ src/Xinax/LaravelGettext/Composers/LanguageSelector.php
@@ -79,9 +79,9 @@ class LanguageSelector
             if (array_key_exists($locale, $this->labels)) {
                 $localeLabel = $this->labels[$locale];
             }
-            $link = '<a href="/lang/' . $locale . '" class="' . $locale . '">' . $localeLabel . '</a>';
+            $link = '<a href="/lang/' . $locale . '" class="' . $locale . '"><i class="fa fa-language"> ' . $localeLabel . '</a>';
            
-            $html .= '<li><i class="fa fa-language"> ' . $link . '</li>';
+            $html .= '<li>' . $link . '</li>';
         }
 
 
